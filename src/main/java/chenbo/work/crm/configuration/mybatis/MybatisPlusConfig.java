a0ee7cbd96772d0ef3a8c7f65fa9bc962f768618
ChenBo <hopetolight@outlook.com>
2019-05-09 00:26:52 +0800

    配置工程环境

---
+++ src/main/java/chenbo/work/crm/configuration/mybatis/MybatisPlusConfig.java
@@ -0,0 +1,31 @@
+package chenbo.work.crm.configuration.mybatis;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+@MapperScan("chenbo.work.crm.*.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+
+    }
+
+
+    /**
+     * 注入sql注入器
+     */
+    @Bean
+    public ISqlInjector sqlInjector(){
+        return new LogicSqlInjector();
+    }
+}
