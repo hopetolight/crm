0c541d2f2249be6ce6a073b9a947909202ea2ec1
ChenBo <hopetolight@outlook.com>
2019-05-10 00:43:16 +0800

    配置工程前端公共加载

---
+++ src/main/java/chenbo/work/crm/web/core/controller/IndexController.java
@@ -0,0 +1,13 @@
+package chenbo.work.crm.web.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/","/index","/login"})
+    public String index(){
+        return "login";
+    }
+}
---
+++ src/main/resources/static/core/common/js/common.js
@@ -0,0 +1,6 @@
+//TODO 动态获取根目录路径
+/*&lt;![CDATA[*/
+const basePath = /*[[@{/}]]*/ '';
+const contextPath = /*[[ ${#httpServletRequest.getContextPath()} ]]*/ '';
+/*]]>*/
+
